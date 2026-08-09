314a24bdfe35783fefa3ca7f4da2f488e9f35709
Guilherme Machado <gui.mp@gmail.com>
2026-08-09 20:03:38 -0300

    .

--- airflow/dags/test_gcp_connection.py
+++ airflow/dags/test_gcp_connection.py
@@ -4,11 +4,12 @@ import pendulum
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from airflow.providers.google.common.hooks.base_google import GoogleBaseHook
 from airflow.sdk import dag, task
-from airflow.sdk import Variable
 from google.auth.transport.requests import Request
 
 
-DEFAULT_GCP_CONN_ID = "google_cloud_default"
+GCP_CONN_ID = "google_cloud_default"
+PROJECT_ID = "case-grupo-otg1"
+LOCATION = "us-central1"
 
 
 @dag(
@@ -23,17 +24,16 @@ def test_gcp_connection():
 
     @task
     def check_google_credentials() -> str:
-        gcp_conn_id = Variable.get("case_gcp_conn_id", default=DEFAULT_GCP_CONN_ID)
-        hook = GoogleBaseHook(gcp_conn_id=gcp_conn_id)
+        hook = GoogleBaseHook(gcp_conn_id=GCP_CONN_ID)
         credentials = hook.get_credentials()
         credentials.refresh(Request())
         return "Credenciais Google Cloud carregadas e token atualizado com sucesso."
 
     test_bigquery = BigQueryInsertJobOperator(
         task_id="test_bigquery_select_1",
-        gcp_conn_id="{{ var.value.get('case_gcp_conn_id', 'google_cloud_default') }}",
-        project_id="{{ var.value.get('case_gcp_project_id', 'case-grupo-otg1') }}",
-        location="{{ var.value.get('case_bigquery_location', 'us-central1') }}",
+        gcp_conn_id=GCP_CONN_ID,
+        project_id=PROJECT_ID,
+        location=LOCATION,
         configuration={
             "query": {
                 "query": "select 1 as ok, current_timestamp() as tested_at",
